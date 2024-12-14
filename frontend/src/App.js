0b5eebb201b6319dad2f07465f756b8db82a794e
Arirzwn <aririzwan0@gmail.com>
2024-12-15 00:10:17 +0700

    fix: detail article view in dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/diagnosa" element={<Diagnosa />} />
         <Route path="/result-diagnosa" element={<ResultDiagnosa />} />
-        <Route path="/clinic" element={<Clinic />} /> {/* Conflict from clinic branch */}
+        <Route path="/clinic" element={<Clinic />} />{' '}
+        {/* Conflict from clinic branch */}
         <Route path="/artikel" element={<Artikel />} />
         <Route path="/artikel-detail/:id" element={<Detail />} />
         <Route path="/rumah-sakit" element={<RumahSakit />} />
@@ -40,14 +41,27 @@ function App() {
         <Route path="/pengajuan" element={<Pengajuan />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<KlinikDashboard />} /> {/* Conflict from main branch */}
+        <Route path="/dashboard" element={<KlinikDashboard />} />{' '}
+        {/* Conflict from main branch */}
         <Route path="/dashboard/rujukan" element={<Rujukan />} />
         <Route path="/dashboard/profile" element={<Profil />} />
         <Route path="/dashboard/artikel" element={<KlinikArtikel />} />
-        <Route path="/dashboard/artikel/tambah-artikel" element={<BuatArtikel />} />
-        <Route path="/dashboard/artikel/edit-artikel/:id" element={<UpdateArtikel />} />
-        <Route path="/dashboard/artikel/detail" element={<DetailArtikel />} />
-        <Route path="/dashboard/rujukan/tambah-rujukan" element={<BuatRujukan />} />
+        <Route
+          path="/dashboard/artikel/tambah-artikel"
+          element={<BuatArtikel />}
+        />
+        <Route
+          path="/dashboard/artikel/edit-artikel/:id"
+          element={<UpdateArtikel />}
+        />
+        <Route
+          path="/dashboard/artikel/detail/:id"
+          element={<DetailArtikel />}
+        />
+        <Route
+          path="/dashboard/rujukan/tambah-rujukan"
+          element={<BuatRujukan />}
+        />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/klinikPage/DetailArtikel.jsx
+++ frontend/src/klinikPage/DetailArtikel.jsx
@@ -1,52 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import SidebarKlinik from '../components/sidebarKlinik';
-import Artikel1 from "../images/artikel.png";
-import Artikel2 from "../images/artikel2.jpg";
+import Artikel1 from '../images/artikel.png';
 
 function DetailArtikel() {
+  const { id } = useParams();
+  const [article, setArticle] = useState(null);
+
+  useEffect(() => {
+    const fetchArticle = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/api/articles/${id}`
+        );
+        setArticle(response.data);
+      } catch (error) {
+        console.error('Error fetching article:', error);
+      }
+    };
+    fetchArticle();
+  }, [id]);
+
+  if (!article) {
     return (
-        <SidebarKlinik>
-                <div className=" h-100" style={{minWidth: "1200px"}}>
-                    <div className="container">
-                                <section className="bg-light shadow-md">
-                                    <h1 className="fw-bolder fs-3 text-center mt-4">Anemia</h1>
-                                    <img className="rounded mx-auto d-block mt-4 mb-4 artikel-detail-img" src={Artikel2} alt="" />
-                                </section>
-                                <section className="bg-light shadow-md">
-                                    <div className="row">
-                                        <div className="col-1" style={{width: "50px"}}>
-                                            <img className="artikel-detail-profil" src={Artikel1} alt="" />
-                                        </div>
-                                        <div className="col-1" style={{width: "110px"}}>
-                                            <h3 className="fw-bolder artikel-detail-username">
-                                                Dr. Ricard
-                                            </h3>
-                                        </div>
-                                        <div className="col-1" style={{width: "5px"}}>
-                                            <h3 className="artikel-detail-hari">
-                                                ~
-                                            </h3>
-                                        </div>
-                                        <div className="col-4">
-                                            <h3 className="artikel-detail-hari">
-                                                1 Hari Yang Lalu
-                                            </h3>
-                                        </div>
-                                    </div>
-                                        <div className="mt-3">
-                                            <h2 className="fw-bolder fs-4 mb-4">Panduan Sederhana Menjaga Pola Makan Sehat untuk Hidup Lebih Berkualitas</h2>
-                                            <h2 className="fs-6 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tempora necessitatibus amet, totam laboriosam nesciunt est libero nobis. Sint ipsum laboriosam unde quisquam ad mollitia, recusandae aliquid? Nobis, ipsa vitae.</h2>
-                                            <h2 className="fs-6 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tempora necessitatibus amet, totam laboriosam nesciunt est libero nobis. Sint ipsum laboriosam unde quisquam ad mollitia, recusandae aliquid? Nobis, ipsa vitae.</h2>
-                                            <h2 className="fs-6 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tempora necessitatibus amet, totam laboriosam nesciunt est libero nobis. Sint ipsum laboriosam unde quisquam ad mollitia, recusandae aliquid? Nobis, ipsa vitae.</h2>
-                                            <h2 className="fs-6 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tempora necessitatibus amet, totam laboriosam nesciunt est libero nobis. Sint ipsum laboriosam unde quisquam ad mollitia, recusandae aliquid? Nobis, ipsa vitae.</h2>
-                                            <h2 className="fs-6 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tempora necessitatibus amet, totam laboriosam nesciunt est libero nobis. Sint ipsum laboriosam unde quisquam ad mollitia, recusandae aliquid? Nobis, ipsa vitae.</h2>
-                                            <h2 className="fs-6 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tempora necessitatibus amet, totam laboriosam nesciunt est libero nobis. Sint ipsum laboriosam unde quisquam ad mollitia, recusandae aliquid? Nobis, ipsa vitae.</h2>
-                                        </div>
-                                </section>
-                            </div>
-                </div>
-            </SidebarKlinik>
+      <SidebarKlinik>
+        <div className="text-center">Loading...</div>
+      </SidebarKlinik>
     );
+  }
+
+  return (
+    <SidebarKlinik>
+      <div className="h-100" style={{ minWidth: '1200px' }}>
+        <div className="container">
+          <section className="bg-light shadow-md">
+            <h1 className="fw-bolder fs-3 text-center mt-4">{article.title}</h1>
+          </section>
+          <section className="bg-light shadow-md">
+            <div className="row">
+              <div className="col-1" style={{ width: '50px' }}>
+                <img className="artikel-detail-profil" src={Artikel1} alt="" />
+              </div>
+              <div className="col-1" style={{ width: '110px' }}>
+                <h3 className="fw-bolder artikel-detail-username">
+                  {article.author}
+                </h3>
+              </div>
+              <div className="col-4">
+                <h3 className="artikel-detail-hari">
+                  {new Date(article.date).toLocaleDateString()}
+                </h3>
+              </div>
+            </div>
+            <div className="mt-3">
+              <h2 className="fw-bolder fs-4 mb-4">{article.description}</h2>
+              <div className="fs-6 mb-3">{article.content}</div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </SidebarKlinik>
+  );
 }
 
 export default DetailArtikel;
