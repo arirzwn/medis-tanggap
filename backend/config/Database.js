8f079abd8d7338bbd0931bd03b01fd5db061dcbc
davano <yustinamalia22@gmail.com>
2024-12-18 22:28:20 +0700

    name db

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('medis_tanggap2', 'root', '', {
+const db = new Sequelize('medis_tanggap', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
 });
