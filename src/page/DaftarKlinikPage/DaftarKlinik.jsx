c987d0e98854ad9bb81704b84dc0ac8431bfe5e7
Arirzwn <aririzwan0@gmail.com>
2024-12-21 05:56:02 +0700

    Add Babel plugin for private property support in devDependencies

--- src/page/DaftarKlinikPage/DaftarKlinik.jsx
+++ src/page/DaftarKlinikPage/DaftarKlinik.jsx
@@ -1,24 +1,19 @@
-import Footer from "../../components/footer";
-import Navbar from "../../components/navbar";
-import { useNavigate } from "react-router-dom";
+import Footer from '../../components/footer';
+import Navbar from '../../components/navbar';
 
 function Pengajuan() {
-    const navigate = useNavigate()
-    return(
-        <>
-        <Navbar />
-        <div id="rs">
-            <div className="header text-center p-4">
-                <p>Pengajuan Klinik</p>
-            </div>
-            <main className="container flex-container p-4">
-                    
-                
-            </main>
+  return (
+    <>
+      <Navbar />
+      <div id="rs">
+        <div className="header text-center p-4">
+          <p>Pengajuan Klinik</p>
         </div>
-        <Footer />
-        </>
-    )
+        <main className="container flex-container p-4"></main>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
-export default Pengajuan
+export default Pengajuan;
