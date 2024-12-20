a95616ee88a1ce55a8a5cb532974085ac599234d
Arirzwn <aririzwan0@gmail.com>
2024-12-21 04:07:43 +0700

    Remove unused files and dependencies from the project

--- backend/routes/SymptomsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const symptomController = require('../controllers/SymptomsController');
-
-// Mendefinisikan route untuk mengambil gejala
-router.get('/symptoms', symptomController.getSymptoms);
-
-module.exports = router;
