26d62f18c86dbc66b0461c732c3cdbcd7b0cb10b
Arirzwn <aririzwan0@gmail.com>
2024-12-21 15:02:49 +0700

    refactor database configuration to use environment variables

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,10 +1,19 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const db = new Sequelize('medis_tanggap', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false,
-});
+dotenv.config();
+
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+  }
+);
 
 // Function to initialize database
 const initializeDatabase = async () => {
